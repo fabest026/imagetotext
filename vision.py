383adf404bbd0b41f902e9eaa49f086b34051b57
Muhammad Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2024-04-13 15:21:54 +0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -9,7 +9,7 @@ from PIL import Image
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # function to load Gemini Pro model and get reponses
-model=genai.GenerativeModel("gemini-pro-vision")
+model=genai.GenerativeModel("gemini-1.5-pro-latest")
 def get_gemini_response(input,image):
     if input!="":
         response=model.generate_content([input,image])
