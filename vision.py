68bc622a52d15a45d1394d17880619c65d5faca1
Farhan Akbar <161922838+fabest026@users.noreply.github.com>
2024-08-08 11:03:35 +0500

    Update vision.py

--- vision.py
+++ vision.py
@@ -22,7 +22,7 @@ def get_gemini_response(input,image):
 # Navbar
 st.set_page_config(
     page_title="Vision AI",
-    page_icon="circle-music-note",
+    page_icon="📲",
     layout="centered",
     initial_sidebar_state="collapsed",
 )
